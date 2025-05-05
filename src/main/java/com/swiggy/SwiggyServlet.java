d128111fe53e266539ddf81f9282131c90d84da9
Nitturu Nagaswetha <94812103+Naga123swetha@users.noreply.github.com>
2025-05-05 11:28:27 +0530

    Create SwiggyServlet.java

---
+++ src/main/java/com/swiggy/SwiggyServlet.java
@@ -0,0 +1,21 @@
+package com.swiggy;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class SwiggyServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html><head><title>Swiggy</title></head>");
+        out.println("<body style='font-family:sans-serif;text-align:center;margin-top:50px;'>");
+        out.println("<h1>Welcome to Swiggy Web Application</h1>");
+        out.println("<p>Order food online from your favorite restaurants!</p>");
+        out.println("</body></html>");
+    }
+}
